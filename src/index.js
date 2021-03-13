adb6d93992ef359d5f404bc65247d510df140991
backspacev9 <backspacev9@gmail.com>
2021-03-14 02:05:11 +0300

    refactor:complete task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  var newArr = [];
+  if (matrix){
+    for (var i = 0; i < matrix.length; i++){
+      if ((i+1)%2 == 0) {
+        matrix[i].reverse(); 
+      } 
+        newArr =  newArr.concat(matrix[i]);
+    }
+    return newArr;
+  } else {return []; } 
 }
